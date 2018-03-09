d384d7d737898434403223c8c1036b7c80b690a3
withsumanth <withsumanth@gmail.com>
2018-03-08 22:38:26 -0500

    updated

--- insert.php
+++ insert.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
